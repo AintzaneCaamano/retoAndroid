b8bab0f0ba9be66805399a175f06df5504a02f81
MarkelGarcia <markel.garciari@elorrieta-errekamari.com>
2021-12-17 15:21:19 +0100

    Creacion de las activities

---
+++ Euskalmet/app/src/main/java/com/example/euskalmet/DetailEspacioNaturalActivity.java
@@ -0,0 +1,14 @@
+package com.example.euskalmet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailEspacioNaturalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_espacio_natural);
+    }
+}
---
+++ Euskalmet/app/src/main/java/com/example/euskalmet/DetailMunicipioActivity.java
@@ -0,0 +1,14 @@
+package com.example.euskalmet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailMunicipioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_municipio);
+    }
+}
