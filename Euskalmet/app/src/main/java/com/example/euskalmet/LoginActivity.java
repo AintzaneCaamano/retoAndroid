c55b3c94f369f1eec85077adc95d3f9ad160e6f6
MarkelGarcia <markel.garciari@elorrieta-errekamari.com>
2021-12-17 15:58:07 +0100

    Diseño Login y register

--- Euskalmet/app/src/main/java/com/example/euskalmet/LoginActivity.java
+++ Euskalmet/app/src/main/java/com/example/euskalmet/LoginActivity.java
@@ -2,13 +2,38 @@ package com.example.euskalmet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnRegister = findViewById(R.id.btn_register);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToOperaciones = new Intent(LoginActivity.this, ActivityOperaciones.class);
+                startActivity(goToOperaciones);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToRegister = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(goToRegister);
+            }
+        });
     }
 }
--- Euskalmet/app/src/main/java/com/example/euskalmet/RegisterActivity.java
+++ Euskalmet/app/src/main/java/com/example/euskalmet/RegisterActivity.java
@@ -2,13 +2,28 @@ package com.example.euskalmet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button btnRegister2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnRegister2 = findViewById(R.id.btn_register2);
+
+        btnRegister2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(goToLogin);
+            }
+        });
     }
 }
