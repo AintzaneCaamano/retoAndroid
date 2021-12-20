7a0a2577fac9946b4692d5ebe0c6bf507891d980
MarkelGarcia <markel.garciari@elorrieta-errekamari.com>
2021-12-20 18:56:03 +0100

    Diseño + navegacion + Strigs entre Activities

--- Euskalmet/app/src/main/java/com/example/euskalmet/ActivityOperaciones.java
+++ Euskalmet/app/src/main/java/com/example/euskalmet/ActivityOperaciones.java
@@ -2,13 +2,64 @@ package com.example.euskalmet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityOperaciones extends AppCompatActivity {
+
+    Button btnMunicipios;
+    Button btnEspaciosNaturales;
+    Button btnFavoritos;
+    Button btnRanking;
+    Button btnCambiarIdioma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operaciones);
+
+        btnMunicipios = findViewById(R.id.Operac_btnMunicipios);
+        btnEspaciosNaturales = findViewById(R.id.Operac_btnEspacios);
+        btnFavoritos = findViewById(R.id.Operac_btnFavs);
+        btnRanking = findViewById(R.id.Operac_btnTop);
+        btnCambiarIdioma = findViewById(R.id.Operac_changeLang);
+
+        //falta la funcionalidad de cambiar de idioma
+
+        //Cada intent va a mandar informacion diferente, solo que por ahora no tenemos esa info
+        btnMunicipios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent MunicipiosToInfoActivity = new Intent(ActivityOperaciones.this, InfoActivity.class);
+                startActivity(MunicipiosToInfoActivity);
+            }
+        });
+
+        btnEspaciosNaturales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent EspaciosNatToInfoActivity = new Intent(ActivityOperaciones.this, InfoActivity.class);
+                startActivity(EspaciosNatToInfoActivity);
+            }
+        });
+
+        btnFavoritos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent FavoritosToInfoActivity = new Intent(ActivityOperaciones.this, InfoActivity.class);
+                startActivity(FavoritosToInfoActivity);
+            }
+        });
+
+        btnRanking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent RankingToInfoActivity = new Intent(ActivityOperaciones.this, InfoActivity.class);
+                startActivity(RankingToInfoActivity);
+            }
+        });
+
     }
 }
--- Euskalmet/app/src/main/java/com/example/euskalmet/InfoActivity.java
+++ Euskalmet/app/src/main/java/com/example/euskalmet/InfoActivity.java
@@ -2,13 +2,61 @@ package com.example.euskalmet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class InfoActivity extends AppCompatActivity {
+
+    TextView textView_InfoTitle;
+    TextView textView_buscar;
+    EditText editTextBusqueda;
+    ListView listViewInfo;
+    Button btnVolverInfo;
+    Button btnProvisionalDetailMunicipios;
+    Button btnProvisionalDetailEspacioNatural;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        textView_InfoTitle = findViewById(R.id.textView_InfoTitle);
+        textView_buscar = findViewById(R.id.textView_buscar);
+        editTextBusqueda = findViewById(R.id.editText_busqueda);
+        listViewInfo = findViewById(R.id.listView_Info);
+        btnVolverInfo = findViewById(R.id.btn_volverInfo);
+        btnProvisionalDetailMunicipios = findViewById(R.id.btn_provisionalDetailMunicipios);
+        btnProvisionalDetailEspacioNatural = findViewById(R.id.btn_provisionalDetailEspacioNatural);
+
+        btnVolverInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToOperaciones = new Intent(InfoActivity.this, ActivityOperaciones.class);
+                startActivity(goToOperaciones);
+            }
+        });
+
+        btnProvisionalDetailMunicipios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToDetailMunicipio = new Intent(InfoActivity.this, DetailMunicipioActivity.class);
+                startActivity(goToDetailMunicipio);
+            }
+        });
+
+        btnProvisionalDetailEspacioNatural.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToDetailEspacioNatural = new Intent(InfoActivity.this, DetailEspacioNaturalActivity.class);
+                startActivity(goToDetailEspacioNatural);
+            }
+        });
+
+
     }
 }
