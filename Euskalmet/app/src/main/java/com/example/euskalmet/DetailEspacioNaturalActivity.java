9efdf90b4c1f1af1c9dfae35c3bfdaed92ecf084
MarkelGarcia <club penguin7>
2021-12-21 17:33:27 +0100

    Todo menos mapa de geolocalizacion de detail Espacio natural

--- Euskalmet/app/src/main/java/com/example/euskalmet/DetailEspacioNaturalActivity.java
+++ Euskalmet/app/src/main/java/com/example/euskalmet/DetailEspacioNaturalActivity.java
@@ -2,13 +2,31 @@ package com.example.euskalmet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DetailEspacioNaturalActivity extends AppCompatActivity {
+
+    TextView textViewNombreEspacioNatural;
+    Button btnVolverDetailEspacioNatural;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_espacio_natural);
+
+        textViewNombreEspacioNatural = findViewById(R.id.textView_NombreEspacioNatural);
+        btnVolverDetailEspacioNatural = findViewById(R.id.btn_volverDetailEspacioNatural);
+
+        btnVolverDetailEspacioNatural.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToInfo = new Intent(DetailEspacioNaturalActivity.this, InfoActivity.class);
+                startActivity(goToInfo);
+            }
+        });
     }
 }
--- Euskalmet/app/src/main/java/com/example/euskalmet/DetailMunicipioActivity.java
+++ Euskalmet/app/src/main/java/com/example/euskalmet/DetailMunicipioActivity.java
@@ -2,13 +2,33 @@ package com.example.euskalmet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DetailMunicipioActivity extends AppCompatActivity {
+
+    TextView textViewNombreMunicipio;
+    Button btnVolverDetailMunicipio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_municipio);
+
+        textViewNombreMunicipio = findViewById(R.id.textView_NombreEspacioNatural);
+        btnVolverDetailMunicipio = findViewById(R.id.btn_volverDetailEspacioNatural);
+
+        btnVolverDetailMunicipio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToInfo = new Intent(DetailMunicipioActivity.this, InfoActivity.class);
+                startActivity(goToInfo);
+            }
+        });
+
+
     }
 }
