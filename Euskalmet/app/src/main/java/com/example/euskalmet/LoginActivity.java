a676aac061ad4be0baa92602bd53e7c9f881908d
ikerisasi <iker_isasian@elorrieta-errekamari.com>
2022-01-28 16:26:41 +0100

    apariencia + share

--- Euskalmet/app/src/main/java/com/example/euskalmet/LoginActivity.java
+++ Euskalmet/app/src/main/java/com/example/euskalmet/LoginActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -91,6 +94,22 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(goToRegister);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.share_menu){
+
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 
     public boolean isNumeric(String numero){
--- Euskalmet/app/src/main/java/com/example/euskalmet/MainActivity.java
+++ Euskalmet/app/src/main/java/com/example/euskalmet/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
